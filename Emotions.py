815a5f2760f808bb93d163cbbc946160ef0287e4
Nehal70 <63651397+Nehal70@users.noreply.github.com>
2024-10-26 20:22:23 -0400

    added emotions.py

---
+++ Emotions.py
@@ -0,0 +1,51 @@
+Emotions = {
+    "happy": {
+            "danceability": [0.7, 1.0],
+            "Energy": [0.7, 1.0],
+            "Valency": [0.5, 1.0],
+            "Tempo": [120,200],
+            "Liveliness": [0.5, 1.0]
+    },
+    "Sad" : {
+        "danceability": [0.0, 0.3],
+        "Energy": [0.0, 0.3],
+        "Valency": [0.0, 0.5],
+        "Tempo": [60,120],
+        "Liveliness": [0.0, 0.5]
+    },
+    "Fear" : {
+        "danceability": [0.7, 1.0],
+        "Energy": [0.7, 1.0],
+        "Valency": [0.5, 1.0],
+        "Tempo": [80, 140],
+        "Liveliness": [0.0, 0.5]
+    },
+    "Anger" : {
+        "danceability": [0.7, 1.0],
+        "Energy": [0.7, 1.0],
+        "Valency": [0.5, 1.0],
+        "Tempo": [100, 180],
+        "Liveliness": [0.5, 1.0]
+    },
+    "Neutral" : {
+        "danceability": [0.7, 1.0],
+        "Energy": [0.7, 1.0],
+        "Valency": [0.5, 1.0],
+        "Tempo": [90, 120],
+        "Liveliness": [0.5, 1.0]
+    },
+    "Calm" : {
+        "danceability": [0.2, 0.4],
+        "Energy": [0.2, 0.4],
+        "Valency": [0.6, 0.9],
+        "Tempo": [50, 80],
+        "Liveliness": [0.2, 0.4]
+    },
+    "Mystery" : {
+            "danceability": [0.3, 0.5],
+            "Energy": [0.3, 0.7],
+            "Valency": [0.0, 0.4],
+            "Tempo": [80, 140],
+            "Liveliness": [0.2, 0.5]
+    },
+}
